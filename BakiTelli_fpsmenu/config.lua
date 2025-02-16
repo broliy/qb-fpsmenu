bf898aaa58a1a39fe456166838d5bb95c844fd6c
broliy <ademadem1234adml@gmail.com>
2025-02-16 05:18:31 +0100

    Add files via upload

---
+++ BakiTelli_fpsmenu/config.lua
@@ -0,0 +1,13 @@
+
+Config = {}
+
+
+Config.ServerName = "la casa " -- # SERVER NAME # --
+
+Config.SubName = "FPS MENU" -- # NAME # --
+
+Config.Discord = "https://discord.com/invite/cXf5GND7NG" -- # DISCORD URL # --
+
+Config.Web = "https://discord.com/invite/cXf5GND7NG " -- # WEB URL # --
+
+Config.Command = "fps" -- # FPS MENU OPEN COMMAND # --
